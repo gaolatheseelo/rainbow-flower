a58ba01d156cc5f64584709beb09607bc5c8b5e0
Gaolathe Seelo <gaolatlhe.seelo@vcontractor.co.za>
2022-06-19 18:16:24 +0200

    init commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+canvas.width=window.innerWidth;
+canvas.height =window.innerHeight;
+ctx.globalCompositeOperation='destination-over';
+// let positionX = canvas.width / 2;
+// let positionY = canvas.height / 2;
+// let angle= 0;
+
+let number = 0;
+let scale = 10;
+let hue = Math.random() * 360;
+
+function drawFlower(){ 
+    let angle= number * 0.4;
+    let radius = scale * Math.sqrt(number);
+    let positionX = radius * Math.sin(angle) + canvas.width / 2;
+    let positionY = radius * Math.cos(angle) + canvas.height / 2;
+
+    ctx.fillStyle = 'hsl('+ hue +',100%,50%)';
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 5;
+    ctx.beginPath();
+    ctx.arc(positionX,positionY,8,0,Math.PI*2);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+    number++;
+    hue++;
+
+}
+
+function animate(){
+    //draw each frame
+   //ctx.clearRect(0,0,canvas.width,canvas.height)
+   
+    // positionX+=5 * Math.sin(angle);
+    // positionY+=5 * Math.cos(angle);
+    // angle+=0.1;
+    if (number > 920) return;
+    drawFlower();
+    requestAnimationFrame(animate)
+}
+
+animate();
